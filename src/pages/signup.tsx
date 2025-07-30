1d51d80c8717efe1f08a468742dbc6e225db1460
Suraj Suryawanshi <surajsuryawanshi2424@gmail.com>
2025-07-30 11:34:57 +0530

    url fixes

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -120,7 +120,7 @@ export default function SignUpPage() {
       return;
     }
 
-    const url = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+    const url = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 
     setIsSubmitting(true);
     try {
